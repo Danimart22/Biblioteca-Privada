27ff12b0797e63c6b68c1be0e29717f4874ce3f7
Danimart22 <danimartinez12@live.com>
2025-03-12 08:35:49 -0500

    Actualización con repositorio anterior, clase Usuario y Libro

---
+++ Biblioteca-Privada/Libro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Libro
+{
+
+    private int ID;
+    private string Titulo;
+    private string Autor;
+    private int precio;
+    private int stock;
+
+    public Libro(int ID, string Titulo, string Autor, int precio, int stock)
+    {
+        this.ID = ID;
+        this.Titulo = Titulo;
+        this.Autor = Autor;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+
+
+
+}
