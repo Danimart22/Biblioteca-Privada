68dc7597517ca8e809caefe81340993aaf3bdc3b
Danimart22 <danimartinez12@live.com>
2025-03-12 09:09:33 -0500

    Conversión de Usuario a Interface e implementación clase Cliente que hereda de usuario

---
+++ Biblioteca-Privada/Cliente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Privada
+{
+    internal class Cliente : IUsuario
+    {
+        public Cliente(int id, string nombre, string email, string clave, double saldo)
+        {
+            Id = id;
+            Nombre = nombre;
+            Email = email;
+            Clave = clave;
+            Saldo = saldo;
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Clave { get; set; }
+        public double Saldo { get; set; }
+
+        public void verLibros()
+        {
+
+        }
+        public Boolean verificarContraseña(string C)
+        {
+            if (C.Equals(Clave))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- Biblioteca-Privada/Pedido.cs
+++ Biblioteca-Privada/Pedido.cs
@@ -9,12 +9,12 @@ namespace Biblioteca_Privada
     internal class Pedido
     {
         private int id;
-        private Usuario usuario;
+        private IUsuario usuario;
         private List<Libro> libros;
         private double total;
         private DateTime fecha;
 
-        public Pedido(int id, Usuario usuario, List<Libro> libros, double total, DateTime fecha)
+        public Pedido(int id, IUsuario usuario, List<Libro> libros, double total, DateTime fecha)
         {
             this.id = id;
             this.usuario = usuario;
@@ -27,6 +27,6 @@ namespace Biblioteca_Privada
         public List<Libro> Libros { get => libros; set => libros = value; }
         public double Total { get => total; set => total = value; }
         public DateTime Fecha { get => fecha; set => fecha = value; }
-        internal Usuario Usuario { get => usuario; set => usuario = value; }
+        internal IUsuario Usuario { get => usuario; set => usuario = value; }
     }
 }
