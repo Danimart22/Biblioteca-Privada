62693bc4a7ad0a0a5e2895dbef119dd49c976799
Felipe Osorio Restrepo <felipe578av@gmail.com>
2025-03-15 19:21:16 -0500

    Cambios en la clase libro y agregue clase inventario y pago

---
+++ Biblioteca-Privada/Inventario.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Privada
+{
+    internal class Inventario
+    {
+        private List<Libro> libros;
+
+        public Inventario()
+        {
+            libros = new List<Libro>();
+        }
+
+       
+        public void agregarLibro(Libro libro)
+        {
+            libros.Add(libro);
+        }
+
+        
+        public void eliminarLibro(Libro libro)
+        {
+            libros.Remove(libro);
+        }
+        public Libro buscarLibro(string titulo)
+        {
+            foreach (Libro libro in libros)
+            {
+                if (libro.getTitulo().Equals(titulo, StringComparison.OrdinalIgnoreCase))
+                {
+                    return libro;
+                }
+            }
+            return null;
+        }
+
+        public void actualizarStock(Libro libro, int nuevoStock)
+        {
+            Libro libroEncontrado = buscarLibro(libro.getTitulo());
+            if (libroEncontrado != null)
+            {
+                libroEncontrado.setStock(nuevoStock);
+            }
+        }
+
+   
+        public void mostrarLibros()
+        {
+            foreach (Libro libro in libros)
+            {
+                Console.WriteLine(libro.ObtenerDetalles());
+            }
+        }
+    }
+}
