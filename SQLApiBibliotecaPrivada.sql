add16423d57a75d2efab2f4a4166e69548b90f03
Daniel Martinez <167985209+Danimart22@users.noreply.github.com>
2025-05-27 10:50:37 -0500

    Update SQLApiBibliotecaPrivada.sql

--- SQLApiBibliotecaPrivada.sql
+++ SQLApiBibliotecaPrivada.sql
@@ -1,3 +1,5 @@
+create database BibliotecaPrivada;
+use BibliotecaPrivada;
 CREATE TABLE IF NOT EXISTS Libros (
     ID INT PRIMARY KEY,
     Titulo VARCHAR(255) NOT NULL,
@@ -5,7 +7,6 @@ CREATE TABLE IF NOT EXISTS Libros (
     Precio DECIMAL(10,2) NOT NULL,
     Stock INT NOT NULL
 );
-
 DROP TABLE IF EXISTS Cliente;
 CREATE TABLE Cliente (
     ID INT AUTO_INCREMENT PRIMARY KEY,
@@ -14,6 +15,7 @@ CREATE TABLE Cliente (
     Clave VARCHAR(255) NOT NULL,
     Saldo decimal(10,2)
 );
+
 CREATE TABLE IF NOT EXISTS Pedido(
 ID INT PRIMARY KEY,
 IDCliente int,
@@ -22,6 +24,7 @@ Total decimal(10,2),
 Fecha datetime,
 foreign key(IDCliente) references Cliente(ID)
 );
+
 CREATE TABLE IF NOT EXISTS Pago(
 ID INT PRIMARY KEY,
 IDPedido int,
@@ -30,19 +33,23 @@ MetodoPago varchar(20),
 Fecha Datetime,
 NumeroTarjeta varchar(30)
 );
+
 INSERT INTO Cliente (Nombre, Email, Clave, Saldo) VALUES
 ('Laura Gómez', 'laura@example.com', 'clave123', 120.50),
 ('Carlos Méndez', 'carlos@example.com', 'clave456', 75.25),
 ('Sofía Ruiz', 'sofia@example.com', 'clave789', 200.00);
+
 INSERT INTO Pedido (ID, IDCliente, Libros, Total, Fecha) VALUES
 (1, 1, '1,2', 35.49, NOW()),
 (2, 2, '3', 15.50, NOW()),
 (3, 3, '2,3', 28.25, NOW());
+
 INSERT INTO Pago (ID, IDPedido, Monto, MetodoPago, Fecha, NumeroTarjeta) VALUES
 (1,1, 35.49, 'Tarjeta', NOW(), '1234-5678-9012-3456'),
 (2,2, 15.50, 'Efectivo', NOW(), NULL),
 (3,3, 28.25, 'Tarjeta', NOW(), '9876-5432-1098-7654');
-INSERT INTO Libros (ID,Titulo, Autor, Precio, Stock) VALUES 
+
+INSERT INTO Libros (ID, Titulo, Autor, Precio, Stock) VALUES 
 (1,'El Principito', 'Antoine de Saint-Exupéry', 10.99, 15),
 (2,'1984', 'George Orwell', 12.50, 20),
 (3,'Rayuela', 'Julio Cortázar', 13.75, 8);
