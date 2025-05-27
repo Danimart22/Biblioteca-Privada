a06ad064ce4dd228a325bcf6332414716a31ea0f
ramir <danimartinez12@live.com>
2025-05-27 10:33:38 -0500

    Implementación de pedido

--- APIBibliotecaPrivada/DAO/PedidoDAOHelper.cs
+++ APIBibliotecaPrivada/DAO/PedidoDAOHelper.cs
@@ -1,7 +1,7 @@
 ﻿namespace APIBibliotecaPrivada.DAO
 {
     public class PedidoDAOHelper
-    {
+    {//HOLa
         public static string listarPedidos = "SELECT ID, IDCliente, Libros, Total, Fecha FROM BibliotecaPrivada.Pedido";
         public static string guardarPedidos = "Insert into BibliotecaPrivada.Pedido (ID, IDCliente, Libros, Total, Fecha) values (@Id,@IdCliente,@Libros,@Total,@Fecha)";
         public static string actualizarPedidos = "UPDATE BibliotecaPrivada.Pedido set IDCliente = @IdCliente, Libros = @Libros, Total = @Total, Fecha = @Fecha where ID = @Id";
